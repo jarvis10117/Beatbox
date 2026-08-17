0195847437d8556819c1e85844ee4d3ee7767df3
jarvis10117 <pantherbissu@gmail.com>
2026-08-18 01:55:20 +0530

    Gui done

---
+++ src/BeatBox.java
@@ -0,0 +1,108 @@
+import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import static javax.sound.midi.ShortMessage.*;
+
+public class BeatBox {
+
+    private ArrayList<JCheckBox> checkboxList;
+    private Sequencer sequencer;
+    private Sequence sequence;
+    private Track track;
+
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat",
+            "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap",
+            "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga",
+            "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"};
+
+    public static void main(String[] args) {
+        new BeatBox().buildGUI();
+    }
+
+
+
+    public void buildGUI() {
+
+        JFrame frame = new JFrame("Cyber BeatBox");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+
+        JButton start = new JButton("Start");
+        start.addActionListener(e -> System.out.println("start"));
+        buttonBox.add(start);
+
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(e -> System.out.println("stop"));
+        buttonBox.add(stop);
+
+        JButton upTempo = new JButton("Tempo Up");
+        upTempo.addActionListener(e -> System.out.println("tempo up"));
+        buttonBox.add(upTempo);
+
+        JButton downTempo = new JButton("Tempo Down");
+        downTempo.addActionListener(e -> System.out.println("tempo down"));
+        buttonBox.add(downTempo);
+
+        JButton reset = new JButton("Reset");
+        reset.addActionListener(e -> System.out.println("reset"));
+        buttonBox.add(reset);
+
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+
+        for (String instrumentName : instrumentNames) {
+
+            JLabel instrumentLabel = new JLabel(instrumentName);
+            instrumentLabel.setBorder(BorderFactory.createEmptyBorder(4, 1, 4, 1));
+            nameBox.add(instrumentLabel);
+
+        }
+
+        background.add(BorderLayout.EAST, buttonBox);
+        background.add(BorderLayout.WEST, nameBox);
+
+        frame.getContentPane().add(background);
+
+        GridLayout grid = new GridLayout(16, 16);
+        grid.setVgap(1);
+        grid.setHgap(2);
+
+        JPanel mainPanel = new JPanel(grid);
+        background.add(BorderLayout.CENTER, mainPanel);
+
+        checkboxList = new ArrayList<>();
+        for (int i = 0; i < 256; i++) {
+
+            JCheckBox c = new JCheckBox();
+            c.setSelected(false);
+            checkboxList.add(c);
+            mainPanel.add(c);
+
+        }
+
+//        setUpMidi(); Need a method
+
+        frame.setBounds(50, 50, 300, 300);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
